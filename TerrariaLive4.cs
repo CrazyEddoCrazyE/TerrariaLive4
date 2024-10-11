57bfbb3066c2e05f7fb16c1eeb58cb78568cc4b3
Dennis C <dennischoi6@gmail.com>
2024-10-11 12:45:29 +1100

    first commit

---
+++ TerrariaLive4.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace TerrariaLive4
+{
+	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+	public class TerrariaLive4 : Mod
+	{
+
+	}
+}
